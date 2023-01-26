4076e870b728633cbedf0197a5d70e788bb5567d
이하성 <tedsoftj1123@gmail.com>
2023-01-26 15:33:44 +0900

    ✅ :: db connection

---
+++ typeorm/src/app.js
@@ -0,0 +1,3 @@
+import { initApp } from './loader/index.js';
+
+initApp()
---
+++ typeorm/src/loader/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const initApp = () => {
+  
+  const myDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  myDataSource.initialize()
+  .then(() => {
+    console.log('Database Connected ✅');
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+  const app = express();
+
+  app.set('port', process.env.PORT || 3000);
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: false,
+   }),
+  );
+  app.use(morgan('dev'));
+  app.use(
+     cors({
+       origin: [`${process.env.MAIN}`],
+       methods: ['GET', 'POST', 'PUT', 'DELETE'],
+       credentials: true,
+     }),
+  );
+
+  app.listen(app.get("port"), () => {
+     console.log("server started", app.get("port"));
+  });
+}
