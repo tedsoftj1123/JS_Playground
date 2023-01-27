2e9a965aee71be7f326cc2636b270ce2113b56ff
이하성 <tedsoftj1123@gmail.com>
2023-01-27 12:52:19 +0900

    ✅ :: class

--- js_basic/jsClass.js
+++ js_basic/jsClass.js
@@ -27,4 +27,12 @@ console.log(user.getAccountId());
 user.setAccountId('ted');
 console.log(user.getAccountId());
 
-const promise = new Promise()
+//class는 prototype 기반이기에 메소드를 클래스 내에 직접 선언 않고 user프로토 타입에 추가해서 사용할 수 있다.
+User.prototype.hello = function () {
+    console.log('hello');
+}
+const protoUser = new User('tedsoftj1123', '이하성', 1, "d", "asdf");
+protoUser.hello();
+
+console.log(typeof User); // class는 함수와 프로토타입을 이용한 Syntatic Sugar(편의문버)이다.
+console.log(User === User.prototype.constructor); // true
