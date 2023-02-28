4e20e694f3031c6e2eed1159196aeddb2b68f8bb
이하성 <tedsoftj1123@gmail.com>
2023-02-28 23:50:01 +0900

    signup

--- nest/src/domain/user/usecase/signup.usecase.ts
+++ nest/src/domain/user/usecase/signup.usecase.ts
@@ -1,6 +1,12 @@
 import { SignUpRequest } from '../presentation/dto/signup.request';
+import { Inject } from '@nestjs/common';
+import { UserRepository } from '../domain/repository/user.repository';
 
 export class SignUpUseCase {
+  constructor(
+    @Inject('UserRepository')
+    private readonly userRepository: UserRepository,
+  ) {}
   execute(request: SignUpRequest): string {
     return `welcome! ${request.accountId}`;
   }
