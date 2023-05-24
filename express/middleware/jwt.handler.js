3a167375fdc86cdfb146c04297e51eee6f9396b3
tedsoftj1123 <tedsoftj1123@gmail.com>
2023-05-24 12:30:03 +0900

    token error

--- express/middleware/jwt.handler.js
+++ express/middleware/jwt.handler.js
@@ -1,12 +1,18 @@
 import jwt from 'jsonwebtoken';
+import { UnauthorizedException } from '../error/error.js';
 const jwtSecret = process.env.JWT_SECRET;
 
 export const authorize = (req, res, next) => {
-     const token = req.headers.authorization.split('Bearer ')[1];
+     const token = req.headers.authorization?.split('Bearer ')[1];
+
+     if(!token) {
+          throw new UnauthorizedException("Invalid Token");
+     }
+
      try {
           req.currentUserId = jwt.verify(token, jwtSecret)?.sub
           next();
      } catch (err) {
-          next(err);
+          next(new UnauthorizedException(err.message));
      }
 }
